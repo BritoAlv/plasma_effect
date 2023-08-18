47cafe188900a2699ec68c46c2f552b5c140288b
BritoAlv <gomzalezbrito@gmail.com>
2023-08-18 16:53:05 -0400

    refactoring for save bytes

--- Input.asm
+++ Input.asm
@@ -1,17 +1,5 @@
 use16
 
-color1red   equ 255
-color1green equ 103
-color1blue  equ 36
-
-color2red   equ 60
-color2green equ 23
-color2blue  equ 88
-
-color3red   equ 11
-color3green equ 255
-color3blue  equ 120
-
 ; start in VGA Mode
 mov  ax, 0x13
 int  10h
@@ -19,11 +7,9 @@ int  10h
 push 0xA000   ; start of video address
 pop  es
 
-
-
 gameLoop:
     xor bx, bx
-    mov si, 85
+    
     wait_space:
         xor ah, ah
         int 0x16
@@ -31,8 +17,10 @@ gameLoop:
         jne wait_space
 
     execute_action:
+        mov si, 85 ; use SI as an read only register.
         call modify_default_colors
         call modify_vga_palette
+        mov si, 0x3f ; use SI as an read only register.
         call draw_frame
         inc  bx
         xor bh, bh
@@ -57,73 +45,19 @@ draw_frame:
     popa
     ret        
 
+;[Inputs : bl = time, cx = x, dx = y coordinates, si = 63]
 get_color:
     ; function 1: sin(  sqrt( ( x - 180)^2 + (y-180)^2 ) + time )
-    push si
-    mov si, 0x3f
-    push bx
-    push cx
-    push dx
+    ; /////////////// BEGIN //////////////////////////
+    xor ax, ax
 
-    xor di, di
     
-    mov  bx, cx
-    xor  ax, ax
-    mov  al, byte [point1+1]
-    call get_coordinate_distance
-    add  di, ax
+    mov di, point1
+    call .radial_wave
 
-    mov  bx, dx
-    xor  ax, ax
-    mov  al, byte [point1]
-    call get_coordinate_distance
-    add  di, ax
+    mov di, point2
+    call .radial_wave
 
-    call get_sqrt
-
-    pop dx
-    pop cx
-    pop bx
-
-    ; I have at ax sqrt    
-    mov di, ax
-    add di, bx
-    and di, si
-    xor al, al
-    add al, [precomputed_sine_table + di]
-
-    ; function 2: sin( sqrt( ( x - 140)^2 + (y-20)^2 ) + time )
-    push ax
-    push bx
-    push cx
-    push dx
-    xor  di, di
-
-    mov  bx, cx
-    xor  ax, ax
-    mov  al, byte [point2+1]
-    call get_coordinate_distance
-    add  di, ax
-
-    mov  bx, dx
-    xor  ax, ax
-    mov  al, byte [point2]
-    call get_coordinate_distance
-    add  di, ax
-
-    call get_sqrt
-    
-    pop dx
-    pop cx
-    pop bx
-    ; I have at ax sqrt
-    
-    mov di, ax
-    add di, bx
-    and di, si
-    pop ax                                ; restore al
-    add al, [precomputed_sine_table + di] ; mod it
-    
     ; function 3: sin( time*x+y)
     push ax
     mov ax, cx
@@ -151,8 +85,45 @@ get_color:
     add  al, [precomputed_sine_table + di]
     
     inc al
-    pop si
     ret
+
+    .radial_wave:
+        push ax
+        push bx
+        push cx
+        push dx
+
+        mov  bx, cx
+        xor  ax, ax
+        mov  al, [di+1]
+        call get_coordinate_distance    
+        
+        push ax
+
+        mov  bx, dx
+        xor  ax, ax
+        mov  al, [di]
+        call get_coordinate_distance
+        
+        pop di
+        
+        add di, ax
+
+        call get_sqrt
+
+        pop dx
+        pop cx
+        pop bx
+
+        ; I have at ax sqrt    
+        mov di, ax
+        add di, bx
+        and di, si
+        
+        pop ax
+        add al, [precomputed_sine_table + di]
+        ret
+
 
 ;[Input bx = coordinate1, ax = coordinate2]
 ;[Output ax = (coordinate1 - coordinate2)^2]
@@ -169,13 +140,13 @@ get_coordinate_distance:
 ;[ Input di = number, Output ax = root]
 ; this uses Newton method.
 get_sqrt:
-    jz  end_sqrt
+    jz  .end_sqrt
     ; equivalent to do mov ax, 255 but cheaper.
     xor ah, ah
     not al 
     ;
 
-    start_loop:
+    .start_loop:
         mov bx, ax
         xor dx, dx     ; due to the division.
         mov ax, di
@@ -185,28 +156,9 @@ get_sqrt:
         mov cx, ax
         sub cx, bx
         cmp cx, 2
-        ja  start_loop
-    end_sqrt:
+        ja  .start_loop
+    .end_sqrt:
         ret
-
-modify_vga_palette:
-    pusha
-    mov  dx, 0x3c8
-    xor  al, al
-    out  dx, al
-    inc  dx
-    mov  di, colors
-    mov  cx, si
-    call set_color_loop
-    add  di, 3
-    mov  cx, si
-    inc cx
-    call set_color_loop
-    add di, 3
-    mov  cx, si
-    call set_color_loop
-    popa
-    ret
 
 modify_default_colors:    
     mov cx, 0
@@ -259,17 +211,54 @@ modify_default_colors:
     push si
     xor si, si
     mov cx, 3
-    rep call procedure
+    rep call .procedure
     pop si
     pop bx
     ret
 
-    procedure:
+    .procedure:
         mov al, [si + bx]
         mov [di + bx], al
         inc di
         inc si
         ret
+
+; [Inputs: si = 85]
+modify_vga_palette:
+    pusha
+    mov  dx, 0x3c8
+    xor  al, al
+    out  dx, al
+    inc  dx
+    mov  di, colors
+    mov  cx, si
+    call set_color_loop
+    add  di, 3
+    mov  cx, si
+    inc cx
+    call set_color_loop
+    add di, 3
+    mov  cx, si
+    call set_color_loop
+    popa
+    ret
+
+;[ Inputs cx = number_of_iterations, di = start_offset, 
+set_color_loop:
+    push di
+    call .procedure
+    call .procedure
+    call .procedure
+    pop di
+    loop set_color_loop    
+    ret
+
+    .procedure:
+        mov  bh, [di]
+        mov  bl, [di+3]
+        call set_color
+        inc  di
+        ret    
 
 set_color:
     pusha 
@@ -287,34 +276,13 @@ set_color:
     popa
     ret
 
-
-;[ Inputs cx = number_of_iterations, di = start_offset, 
-set_color_loop:
-    push di
-    call sset_color_loop
-    call sset_color_loop
-    call sset_color_loop
-    pop di
-    loop set_color_loop    
-    ret
-
-sset_color_loop:
-    mov  bh, [di]
-    mov  bl, [di+3]
-    call set_color
-    inc  di
-    ret    
-
-
 section.data:
-    ; 64 values between 0 and 64 to be used for four functions
+    ; 64 values between 0 and 64 to be used for four functions.
     precomputed_sine_table: db 32,35,38,41,44,47,49,52,54,56,58,60,61,62,63,63,64,63,63,62,61,60,58,56,54,52,49,47,44,41,38,35,31,28,25,22,19,16,14,11,9,7,5,3,2,1,0,0,0,0,0,1,2,3,5,7,9,11,14,16,19,22,25,28
     point1:                 dw 0xB4B4
     point2:                 dw 0x8C14
 
-    colors: db color1red, color1green, color1blue, color2red, color2green, color2blue, color3red, color3green, color3blue, color1red, color1blue, color1green
-
-    parameters: db 0 
+    colors: db 205, 103, 36, 60, 23, 88, 11, 255, 120, 205, 103, 36
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
